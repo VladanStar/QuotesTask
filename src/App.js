a58a6e2bf9b1bee500c61f7626541d1ee0ed1b2e
unknown <vladancupric@gmail.com>
2022-09-12 18:06:36 +0200

    Add page add quotes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import QuotesData from "./components/data/QuotesData";
 import { useState } from "react";
 // import Card from "./components/shared/Card";
 import QuotesStats from "./components/QuotesStats";
+import QuotesForm from "./components/data/QuotesForm";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Header text="QUOTES" />
 
       <div className="container">
+        <QuotesForm />
         <QuotesStats  quotes={quotes}/>
         <QuotesList quotes={quotes} />
       </div>
---
+++ src/components/data/QuotesForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "../shared/Card";
+
+const QuotesForm = () => {
+  return (
+    <Card>
+      <form>
+        <h2>Add Quotes</h2>
+      </form>
+      
+    </Card>
+  );
+};
+
+export default QuotesForm;
