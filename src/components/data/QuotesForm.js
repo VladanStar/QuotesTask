920d96166a89528a43375b9f24e028e514de473d
unknown <vladancupric@gmail.com>
2022-09-12 18:43:39 +0200

    ADDD

--- src/components/data/QuotesForm.js
+++ src/components/data/QuotesForm.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Card from "../shared/Card";
+import { useState } from "react";
 
 const QuotesForm = () => {
+    const [text, setText] = useState("");
+    const[author, setAuthor] = useState("")
+
+    const handleTextchange =(e)=>{
+        console.log(e.target.value);
+
+    }
   return (
     <Card>
       <form>
         <h2>Add Quotes</h2>
+        <div className="input-group">
+            <input  onChange={handleTextchange} type="text" placeholder="Write a quotes" />
+            <input onChange={handleTextchange} type="text" placeholder="Author" />
+            <button type="submit">Send</button>
+        </div>
       </form>
       
     </Card>
