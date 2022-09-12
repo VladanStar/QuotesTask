1ffd16a569dacfbb9cb1c94911bcce9422e8ba27
VladanStar <vladancupric@gmail.com>
2022-09-13 01:18:39 +0200

    Add about

--- src/App.js
+++ src/App.js
@@ -2,49 +2,46 @@ import Header from "./components/Header";
 import QuotesList from "./components/QuotesList";
 import QuotesData from "./components/data/QuotesData";
 import { useState } from "react";
-import {v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid";
 // import Card from "./components/shared/Card";
 import QuotesStats from "./components/QuotesStats";
 import QuotesForm from "./components/QuotesForm";
 import AboutUs from "./components/AboutUs";
-import {BrowserRouter as Router, Route,Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   const [quotes, setQuotes] = useState(QuotesData);
   const [rating, setRating] = useState("");
-  const addQuote= (newQuote)=>{
-    newQuote.id = uuidv4()
-    console.log(newQuote)
-    setQuotes([newQuote,...quotes])
-
-  }
+  const addQuote = (newQuote) => {
+    newQuote.id = uuidv4();
+    console.log(newQuote);
+    setQuotes([newQuote, ...quotes]);
+  };
 
   return (
-    <>
     <Router>
       <Header text="QUOTES" />
 
       <div className="container">
         <Routes>
-        <Route path="/" element={
-          <>
-     
- 
-      <QuotesForm  handleAdd ={addQuote}/>
-        <QuotesStats  quotes={quotes}/>
-        <QuotesList quotes={quotes} />
-        </>
-        }>
-        </Route>
-        
-        <Route path='/about' element={<AboutUs/>}/>
+          <Route path="/about" element={<AboutUs />} />
         </Routes>
-       
-       
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <QuotesForm handleAdd={addQuote} />
+                <QuotesStats quotes={quotes} />
+                <QuotesList quotes={quotes} />
+              </>
+            }
+          ></Route>
+        </Routes>
       </div>
-      </Router>
-    </>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutUs from "./AboutUs";
 
 const Header = ({ text }) => {
   return (
-    <header>
-      <div className="container">
-     
-        <h2>{text}</h2>
-
-      </div>
-    </header>
+   
+      <header>
+       
+        <div className="container">
+          <h2>{text}</h2>
+        </div>
+      
+      </header>
+    
   );
 };
 
