79938b5f930661c61f3e9deb42d0e90b35420bdd
unknown <vladancupric@gmail.com>
2022-09-12 19:33:25 +0200

    Add

--- src/components/data/QuotesForm.js
+++ src/components/data/QuotesForm.js
@@ -3,24 +3,29 @@ import Card from "../shared/Card";
 import { useState } from "react";
 
 const QuotesForm = () => {
-    const [text, setText] = useState("");
-    const[author, setAuthor] = useState("")
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
 
-    const handleTextchange =(e)=>{
-        console.log(e.target.value);
-
-    }
+  const handleTextchange = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <Card>
       <form>
         <h2>Add Quotes</h2>
         <div className="input-group">
-            <input  onChange={handleTextchange} type="text" placeholder="Write a quotes" />
-            <input onChange={handleTextchange} type="text" placeholder="Author" />
-            <button type="submit">Send</button>
+          <input
+            onChange={handleTextchange}
+            type="text"
+            placeholder="Write a quotes"
+            value={text}
+          />
+          <input onChange={handleTextchange} type="text" placeholder="Author" 
+          value={author}
+          />
+          <button type="submit">Send</button>
         </div>
       </form>
-      
     </Card>
   );
 };
