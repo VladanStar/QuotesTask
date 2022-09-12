147a033718a627c582b091283437d3befd02961f
unknown <vladancupric@gmail.com>
2022-09-12 09:11:16 +0200

    Add buttons

--- src/components/QuotesItem.js
+++ src/components/QuotesItem.js
@@ -2,18 +2,35 @@ import React from "react";
 import { useState } from "react";
 
 const QuotesItem = () => {
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState([
+    {
+        text:"Citat",
+        author:"Robert"
+    }
+  ]);
 
-    const [rating, setRating] = useState(7)
+//   const handleClickPlus = () => {
+//     setRating((prev) => {
+//       return prev + 1;
+//     });
+//   };
+//   const handleClickMinus = () => {
+//     setRating((prev) => {
+//       return prev - 1;
+//     });
+//   };
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">
-        This is an example of a feedback
-        <div className="author-display">Author</div>
+        {text[0].text}
+        <p className="author-display">{text[0].author}</p>
       </div>
-  
+      {/* <button onClick={handleClickPlus}>Click rating</button>
+      <button onClick={handleClickMinus}>Click rating</button> */}
     </div>
-  );
+  )
 };
 
 export default QuotesItem;
