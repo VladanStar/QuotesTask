5d6d3039e978c3ff8b06f8479b26ace44470d2ea
unknown <vladancupric@gmail.com>
2022-09-12 09:30:58 +0200

    add files QuotesData

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Header from "./components/Header";
 import QuotesItem from "./components/QuotesItem";
+import QuotesData from "./components/data/QuotesData";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState(QuotesData);
   return (
     <>
       <Header text="QUOTES" />
---
+++ src/components/data/QuotesData.js
@@ -0,0 +1,45 @@
+const QuotesData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "If people would know how little brain is ruling the world, they would die of fear.",
+    author: "Ivo Andric",
+  },
+  {
+    id: 2,
+    rating: 10,
+    text: "It’s weird how a little is necessary for us to be happy, and even more strange: how often we miss it a little!",
+    author: "Ivo Andric",
+  },
+  {
+    id: 3,
+    rating: 10,
+    text: "Thousands of candles can be lighted from a single candle, and the life of the candle will not be shortened. Happiness never decreases by being shared.",
+    author: "Buddha",
+  },
+  {
+    id: 4,
+    rating: 10,
+    text: "If you pick up a starving dog and make him prosperous he will not bite you. This is the principal difference between a dog and man.",
+    author: "Mark Twain",
+  },
+  {
+    id: 5,
+    rating: 10,
+    text: "Always forgive your enemies; nothing annoys them so much.",
+    author: "Oscar Wilde",
+  },
+  {
+    id: 6,
+    rating: 10,
+    text: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 7,
+    rating: 10,
+    text: "So many books, so little time.",
+    author: "Frank Zappa",
+  },
+];
+export default QuotesData;
