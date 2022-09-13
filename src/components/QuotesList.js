f8f43c8142d165f6456ae782995daf9217a083b4
VladanStar <vladancupric@gmail.com>
2022-09-13 10:04:59 +0200

    Add quotes pagination

--- src/components/QuotesList.js
+++ src/components/QuotesList.js
@@ -1,16 +1,23 @@
 import React from "react";
 import QuotesItem from "./QuotesItem";
 import { useState } from "react";
-
+import ReactPaginate from "react-paginate";
 
 const QuotesList = ({ quotes }) => {
+  const [pageNumber, setPageNumber] = useState(0);
   
+  const quotesPerPage = 5;
+  const pagesVisited = pageNumber * quotesPerPage;
 
   if (!quotes || quotes.length === 0) {
     return <p>No Quotes Yet</p>;
   }
 
- 
+  const pageCount = Math.ceil(quotes.length / quotesPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
   return (
     <div className="feedback-list">
 
@@ -20,7 +27,17 @@ const QuotesList = ({ quotes }) => {
     
       ))}
       
-      
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
     </div>
    
   );
