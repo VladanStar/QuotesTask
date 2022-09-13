3a7c4314a83c5ead4fdb303965f61f8c6feefde0
VladanStar <vladancupric@gmail.com>
2022-09-13 09:56:24 +0200

    New commit about pagination

--- src/components/QuotesList.js
+++ src/components/QuotesList.js
@@ -1,21 +1,16 @@
 import React from "react";
 import QuotesItem from "./QuotesItem";
 import { useState } from "react";
-import Pagination from "./Pagination";
+
 
 const QuotesList = ({ quotes }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(5);
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  
 
   if (!quotes || quotes.length === 0) {
     return <p>No Quotes Yet</p>;
   }
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = quotes.slice(indexOfFirstPost, indexOfLastPost);
+ 
   return (
     <div className="feedback-list">
 
@@ -24,10 +19,7 @@ const QuotesList = ({ quotes }) => {
         
     
       ))}
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={quotes.length}
-        paginate={paginate}/>
+      
       
     </div>
    
